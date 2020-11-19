88328b63f6aca23a13557b32c24c9d45339c032f
firefly1126 <xiaoming1109@gmail.com>
2020-11-19 10:42:18 +0800

    enhance update market event

--- worker/snapshot/market.go
+++ worker/snapshot/market.go
@@ -105,77 +105,6 @@ var handleUpdateMarketEvent = func(ctx context.Context, w *Worker, action core.A
 	}
 
 	symbol := strings.ToUpper(action[core.ActionKeySymbol])
-	initialExchangeRate, e := decimal.NewFromString(action[core.ActionKeyInitExchangeRate])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if initialExchangeRate.LessThanOrEqual(decimal.Zero) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	reserveFactor, e := decimal.NewFromString(action[core.ActionKeyReserveFactor])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if reserveFactor.LessThanOrEqual(decimal.Zero) || reserveFactor.GreaterThanOrEqual(decimal.NewFromInt(1)) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	liquidationIncentive, e := decimal.NewFromString(action[core.ActionKeyLiquidationIncentive])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if liquidationIncentive.LessThan(compound.LiquidationIncentiveMin) || liquidationIncentive.GreaterThan(compound.LiquidationIncentiveMax) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	borrowCap, e := decimal.NewFromString(action[core.ActionKeyBorrowCap])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if borrowCap.LessThan(decimal.Zero) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	collateralFactor, e := decimal.NewFromString(action[core.ActionKeyCollateralFactor])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if collateralFactor.LessThan(decimal.Zero) || collateralFactor.GreaterThan(compound.CollateralFactorMax) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	closeFactor, e := decimal.NewFromString(action[core.ActionKeyCloseFactor])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if closeFactor.LessThan(compound.CloseFactorMin) || closeFactor.GreaterThan(compound.CloseFactorMax) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	baseRate, e := decimal.NewFromString(action[core.ActionKeyBaseRate])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if baseRate.LessThanOrEqual(decimal.Zero) || baseRate.GreaterThanOrEqual(decimal.NewFromInt(1)) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-
-	multiplier, e := decimal.NewFromString(action[core.ActionKeyMultiPlier])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if multiplier.LessThanOrEqual(decimal.Zero) || multiplier.GreaterThanOrEqual(decimal.NewFromInt(1)) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	jumpMultiplier, e := decimal.NewFromString(action[core.ActionKeyJumpMultiPlier])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if jumpMultiplier.LessThanOrEqual(decimal.Zero) || jumpMultiplier.GreaterThanOrEqual(decimal.NewFromInt(1)) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	kink, e := decimal.NewFromString(action[core.ActionKeyKink])
-	if e != nil {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
-	if kink.LessThan(decimal.Zero) || jumpMultiplier.GreaterThanOrEqual(decimal.NewFromInt(1)) {
-		return handleRefundEvent(ctx, w, action, snapshot, core.ErrInvalidAmount)
-	}
 
 	market, e := w.marketStore.FindBySymbol(ctx, symbol)
 	if e != nil {
@@ -188,16 +117,55 @@ var handleUpdateMarketEvent = func(ctx context.Context, w *Worker, action core.A
 		}
 	}
 
-	market.InitExchangeRate = initialExchangeRate
-	market.ReserveFactor = reserveFactor
-	market.LiquidationIncentive = liquidationIncentive
-	market.BorrowCap = borrowCap
-	market.CollateralFactor = collateralFactor
-	market.CloseFactor = closeFactor
-	market.BaseRate = baseRate
-	market.Multiplier = multiplier
-	market.JumpMultiplier = jumpMultiplier
-	market.Kink = kink
+	initialExchangeRate, e := decimal.NewFromString(action[core.ActionKeyInitExchangeRate])
+	if e == nil && initialExchangeRate.GreaterThan(decimal.Zero) {
+		market.InitExchangeRate = initialExchangeRate
+	}
+
+	reserveFactor, e := decimal.NewFromString(action[core.ActionKeyReserveFactor])
+	if e == nil && reserveFactor.GreaterThan(decimal.Zero) && reserveFactor.LessThan(decimal.NewFromInt(1)) {
+		market.ReserveFactor = reserveFactor
+	}
+
+	liquidationIncentive, e := decimal.NewFromString(action[core.ActionKeyLiquidationIncentive])
+	if e == nil && liquidationIncentive.GreaterThanOrEqual(compound.LiquidationIncentiveMin) && liquidationIncentive.LessThanOrEqual(compound.LiquidationIncentiveMax) {
+		market.LiquidationIncentive = liquidationIncentive
+	}
+
+	borrowCap, e := decimal.NewFromString(action[core.ActionKeyBorrowCap])
+	if e == nil && borrowCap.GreaterThanOrEqual(decimal.Zero) {
+		market.BorrowCap = borrowCap
+	}
+
+	collateralFactor, e := decimal.NewFromString(action[core.ActionKeyCollateralFactor])
+	if e == nil && collateralFactor.GreaterThanOrEqual(decimal.Zero) && collateralFactor.LessThanOrEqual(compound.CollateralFactorMax) {
+		market.CollateralFactor = collateralFactor
+	}
+
+	closeFactor, e := decimal.NewFromString(action[core.ActionKeyCloseFactor])
+	if e == nil && closeFactor.GreaterThanOrEqual(compound.CloseFactorMin) && closeFactor.LessThanOrEqual(compound.CloseFactorMax) {
+		market.CloseFactor = closeFactor
+	}
+
+	baseRate, e := decimal.NewFromString(action[core.ActionKeyBaseRate])
+	if e == nil && baseRate.GreaterThan(decimal.Zero) && baseRate.LessThan(decimal.NewFromInt(1)) {
+		market.BaseRate = baseRate
+	}
+
+	multiplier, e := decimal.NewFromString(action[core.ActionKeyMultiPlier])
+	if e == nil && multiplier.GreaterThan(decimal.Zero) && multiplier.LessThan(decimal.NewFromInt(1)) {
+		market.Multiplier = multiplier
+	}
+
+	jumpMultiplier, e := decimal.NewFromString(action[core.ActionKeyJumpMultiPlier])
+	if e == nil && jumpMultiplier.GreaterThanOrEqual(decimal.Zero) && jumpMultiplier.LessThan(decimal.NewFromInt(1)) {
+		market.JumpMultiplier = jumpMultiplier
+	}
+
+	kink, e := decimal.NewFromString(action[core.ActionKeyKink])
+	if e == nil && kink.GreaterThanOrEqual(decimal.Zero) && jumpMultiplier.LessThan(decimal.NewFromInt(1)) {
+		market.Kink = kink
+	}
 
 	if e = w.marketStore.Update(ctx, w.db, market); e != nil {
 		return e
