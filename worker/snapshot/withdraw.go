d4b4d8e49002d3a5eb1b904b1dc6de4f158ced13
firefly1126 <xiaoming1109@gmail.com>
2021-03-29 15:00:32 +0800

    check the market and reserves before withdraw transfer

--- worker/snapshot/withdraw.go
+++ worker/snapshot/withdraw.go
@@ -4,6 +4,7 @@ import (
 	"compound/core"
 	"compound/core/proposal"
 	"context"
+	"errors"
 
 	"github.com/fox-one/pkg/logger"
 	"github.com/fox-one/pkg/store/db"
@@ -14,15 +15,43 @@ func (w *Payee) handleWithdrawEvent(ctx context.Context, p *core.Proposal, req p
 
 	amount := req.Amount.Truncate(8)
 
-	transfer := &core.Transfer{
-		TraceID:   p.TraceID,
-		AssetID:   req.Asset,
-		Amount:    amount,
-		Threshold: 1,
-		Opponents: []string{req.Opponent},
+	// check the market
+	market, isRecordNotFound, e := w.marketStore.Find(ctx, req.Asset)
+	if isRecordNotFound {
+		log.Errorln(errors.New("invalid market"))
+		return nil
+	}
+
+	if e != nil {
+		log.WithError(e).Errorln("find market error")
+		return e
+	}
+
+	// check the amount
+	if amount.GreaterThan(market.Reserves) {
+		log.Errorln("insufficient reserves")
+		return nil
 	}
 
 	return w.db.Tx(func(tx *db.DB) error {
+		// update market total_cash and reserves
+		market.TotalCash = market.TotalCash.Sub(amount)
+		market.Reserves = market.Reserves.Sub(amount)
+
+		if err := w.marketStore.Update(ctx, tx, market); err != nil {
+			log.WithError(err).Errorln("update market error")
+			return err
+		}
+
+		// create transfer
+		transfer := &core.Transfer{
+			TraceID:   p.TraceID,
+			AssetID:   req.Asset,
+			Amount:    amount,
+			Threshold: 1,
+			Opponents: []string{req.Opponent},
+		}
+
 		if err := w.walletStore.CreateTransfers(ctx, w.db, []*core.Transfer{transfer}); err != nil {
 			log.WithError(err).Errorln("wallets.CreateTransfers")
 			return err
