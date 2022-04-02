a39819b6cc0cad8eaf5621d84b45bb227243d10a
Charlie Wu <wu@fox.one>
2022-04-02 10:32:11 +0800

    MOD: fix make proposal

--- worker/payee/proposal_v1.go
+++ worker/payee/proposal_v1.go
@@ -152,7 +152,7 @@ func (w *Payee) buildProposalV1(ctx context.Context, output *core.Output, action
 		return nil, fmt.Errorf("unknown proposal action %d", p.Action)
 	}
 
-	if _, err := mtg.Scan(message, content); err != nil {
+	if err := content.UnmarshalBinary(message); err != nil {
 		log.WithError(err).Debugln("decode proposal content failed")
 	}
 
